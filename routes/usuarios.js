56b2cf0976c9c69d36dc915d5ad71236c11600ec
Santiago Bertorello <santinbertorello@gmail.com>
2025-09-23 11:55:13 -0300

    refactory: mejora en legibilidad de rutas de usuario

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,11 +3,15 @@ import express from 'express';
 import { getUsuarios, getUsuarioConId, addUsuario, editUsuario, deleteUsuario } from '../controllers/usuario.js';
 const usuariosRouter = express.Router();
 
+
 usuariosRouter.get('/', getUsuarios);
-usuariosRouter.get('/:usuario_id', getUsuarioConId);
+
 usuariosRouter.post('/', addUsuario);
-usuariosRouter.put('/:usuario_id', editUsuario);
-usuariosRouter.delete('/:usuario_id',  deleteUsuario );
+
+usuariosRouter.route('/:usuario_id')
+    .get(getUsuarioConId)
+    .put(editUsuario)
+    .delete(deleteUsuario );
 
 export {usuariosRouter};
 
