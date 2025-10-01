17ebb50bd00c8e2d4f7cb77d71bfae2cf61d70bb
Gabriela <gabrielasan07@gmail.com>
2025-09-30 23:05:58 -0300

    fix turnosR.js

--- routes/turnosR.js
+++ routes/turnosR.js
@@ -1,12 +1,12 @@
-import express from "express";
-import { getTurnos, getTurnoConId, addTurno, updateTurno, deleteTurno } from "../controllers/turnos.controller.js";
+import express from 'express';
+import { getTurnos, getTurnoConId, addTurno, updateTurno, deleteTurno } from '../controllers/turnos.js';
 
-const router = express.Router();
+const turnosRouter = express.Router();
 
-router.get("/turnos", getTurnos);
-router.get("/turnos/:id", getTurnoConId);
-router.post("/turnos", addTurno);
-router.put("/turnos/:id", updateTurno);
-router.delete("/turnos/:id", deleteTurno);
+turnosRouter.get('/turnos', getTurnos);
+turnosRouter.get('/turnos/:id', getTurnoConId);
+turnosRouter.post('/turnos', addTurno);
+turnosRouter.put('/turnos/:id', updateTurno);
+turnosRouter.delete('/turnos/:id', deleteTurno);
 
-export default router;
+export { turnosRouter };
