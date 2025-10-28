5d215151721652bb48577866a453899720db4481
Santiago Bertorello <santinbertorello@gmail.com>
2025-10-28 00:28:35 -0300

    validaciones campo json

---
+++ src/middleware/reservasValidaciones/reservasValidator.js
@@ -0,0 +1,26 @@
+import { check } from 'express-validator';
+import { validarCampos } from '../validarCampos.js';
+
+export const validarCreateReserva = [
+  check('fecha_reserva')
+    .notEmpty().withMessage('La fecha de reserva es obligatoria.')
+    .isISO8601().withMessage('Debe tener un formato de fecha válido (YYYY-MM-DD).'),
+
+  check('salon_id')
+    .notEmpty().withMessage('El ID del salón es obligatorio.')
+    .isInt({ gt: 0 }).withMessage('El ID del salón debe ser un número entero positivo.'),
+
+  check('usuario_id')
+    .notEmpty().withMessage('El ID del usuario es obligatorio.')
+    .isInt({ gt: 0 }).withMessage('El ID del usuario debe ser un número entero positivo.'),
+
+  check('turno_id')
+    .notEmpty().withMessage('El ID del turno es obligatorio.')
+    .isInt({ gt: 0 }).withMessage('El ID del turno debe ser un número entero positivo.'),
+
+  check('importe_total')
+    .notEmpty().withMessage('El importe total es obligatorio.')
+    .isFloat({ gt: 0 }).withMessage('El importe total debe ser un número positivo.'),
+
+  validarCampos
+];
