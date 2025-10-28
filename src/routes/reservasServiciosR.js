e3a20b20bc341a3bc97e03b1a05ea59400e8fe59
Victoria Ines <inessavalos14@gmail.com>
2025-10-28 01:06:01 -0300

    REFACTOR: actualizacion y mejoras de las rutas
    Co-authored-by: Gabr!ela. <gabrielasan07@gmail.com>

--- src/routes/reservasServiciosR.js
+++ src/routes/reservasServiciosR.js
@@ -1,14 +1,44 @@
 import express from 'express';
 import reservasServiciosController from '../controllers/reservas_Servicios.js';
+import { validarCreateReservaServicio, validarEditReservaServicio } from '../middleware/serviciosReservasValidator.js';
+import { cacheMinutes} from '../middleware/cache.js';
+import { validarId } from '../middleware/validacionId.js'
 
 
 const reservasServiciosR = express.Router();
 const controller = new reservasServiciosController();
 
-reservasServiciosR.get('/', controller.getReservasServicios);
-reservasServiciosR.get('/:id', controller.getReservaServicioConId);
-reservasServiciosR.post('/',controller.addServicioReserva);
-reservasServiciosR.put('/:id', controller.updateReservaServicio);
-//reservasServiciosR.delete('/reserva_servicio/:id', controller.deleteRerservaServicio);
 
-export {reservasServiciosR};
+reservasServiciosR.get('/',
+  cacheMinutes,
+  controller.getReservasServicios
+);
+
+
+reservasServiciosR.post('/',
+  validarCreateReservaServicio,
+  controller.addServicioReserva
+);
+
+
+reservasServiciosR
+  .route('/:reserva_servicio_id')
+    .get(
+        validarId('reserva_servicio_id'),
+        cacheMinutes,
+        controller.getReservaServicioConId
+    )
+
+    .put(
+        validarId('reserva_servicio_id'),
+        validarEditReservaServicio,
+        controller.updateReservaServicio
+    )
+
+    // .delete(
+    //     validarId,
+    //     controller.
+    // );
+
+
+export { reservasServiciosR };
